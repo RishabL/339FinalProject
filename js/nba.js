be7f7b4e282af69143316f0ccf4a2350992db0b7
RishabL <92960052+RishabL@users.noreply.github.com>
2023-04-21 23:46:20 -0400

    Add files via upload

---
+++ js/nba.js
@@ -0,0 +1,210 @@
+
+document.querySelector("#GA2").addEventListener("focus", function() {
+
+    document.getElementById("GA2").style.color = "green";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#GA2").addEventListener("focusout", function() {
+
+    document.getElementById("GA2").style.color = "black";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#SC2").addEventListener("focus", function() {
+
+    document.getElementById("SC2").style.color = "blue";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#SC2").addEventListener("focusout", function() {
+
+    document.getElementById("SC2").style.color = "black";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#KD2").addEventListener("focus", function() {
+
+    document.getElementById("KD2").style.color = "brown";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#KD2").addEventListener("focusout", function() {
+
+    document.getElementById("KD2").style.color = "black";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#JE2").addEventListener("focus", function() {
+
+    document.getElementById("JE2").style.color = "blue";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#JE2").addEventListener("focusout", function() {
+
+    document.getElementById("JE2").style.color = "black";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#NJ2").addEventListener("focus", function() {
+
+    document.getElementById("NJ2").style.color = "blue";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#NJ2").addEventListener("focusout", function() {
+
+    document.getElementById("NJ2").style.color = "black";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#LD2").addEventListener("focus", function() {
+
+    document.getElementById("LD2").style.color = "blue";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#LD2").addEventListener("focusout", function() {
+
+    document.getElementById("LD2").style.color = "black";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#LJ2").addEventListener("focus", function() {
+
+    document.getElementById("LJ2").style.color = "purple";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#LJ2").addEventListener("focusout", function() {
+
+    document.getElementById("LJ2").style.color = "black";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#JT2").addEventListener("focus", function() {
+
+    document.getElementById("JT2").style.color = "green";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#JT2").addEventListener("focusout", function() {
+
+    document.getElementById("JT2").style.color = "black";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#KL2").addEventListener("focus", function() {
+
+    document.getElementById("KL2").style.color = "blue";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#KL2").addEventListener("focusout", function() {
+
+    document.getElementById("KL2").style.color = "black";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#AD2").addEventListener("focus", function() {
+
+    document.getElementById("AD2").style.color = "purple";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#AD2").addEventListener("focusout", function() {
+
+    document.getElementById("AD2").style.color = "black";
+    
+  
+  
+  
+  
+  
+});
+
+
+
+
+
+
+
+
+
---
+++ js/nfl.js
@@ -0,0 +1,203 @@
+
+//NFL JS
+
+document.querySelector("#PM").addEventListener("focus", function() {
+
+    document.getElementById("PM").style.color = "red";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#PM").addEventListener("focusout", function() {
+
+    document.getElementById("PM").style.color = "black";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#JB").addEventListener("focus", function() {
+
+    document.getElementById("JB").style.color = "brown";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#JB").addEventListener("focusout", function() {
+
+    document.getElementById("JB").style.color = "black";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#JA").addEventListener("focus", function() {
+
+    document.getElementById("JA").style.color = "blue";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#JA").addEventListener("focusout", function() {
+
+    document.getElementById("JA").style.color = "black";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#JH").addEventListener("focus", function() {
+
+    document.getElementById("JH").style.color = "blue";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#JH").addEventListener("focusout", function() {
+
+    document.getElementById("JH").style.color = "black";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#AR").addEventListener("focus", function() {
+
+    document.getElementById("AR").style.color = "green";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#AR").addEventListener("focusout", function() {
+
+    document.getElementById("AR").style.color = "black";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#LA").addEventListener("focus", function() {
+
+    document.getElementById("LA").style.color = "purple";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#LA").addEventListener("focusout", function() {
+
+    document.getElementById("LA").style.color = "black";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#HU").addEventListener("focus", function() {
+
+    document.getElementById("HU").style.color = "green";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#HU").addEventListener("focusout", function() {
+
+    document.getElementById("HU").style.color = "black";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#TL").addEventListener("focus", function() {
+
+    document.getElementById("TL").style.color = "green";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#TL").addEventListener("focusout", function() {
+
+    document.getElementById("TL").style.color = "black";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#MS").addEventListener("focus", function() {
+
+    document.getElementById("MS").style.color = "blue";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#MS").addEventListener("focusout", function() {
+
+    document.getElementById("MS").style.color = "black";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#DP").addEventListener("focus", function() {
+
+    document.getElementById("DP").style.color = "blue";
+    
+  
+  
+  
+  
+  
+});
+document.querySelector("#DP").addEventListener("focusout", function() {
+
+    document.getElementById("DP").style.color = "black";
+    
+  
+  
+  
+  
+  
+});
